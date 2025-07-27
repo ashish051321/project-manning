87faef86c9e33a14b47dad11e816fed5f73c6e60
ashish051321 <ashish.25jl@gmail.com>
2025-07-27 07:02:20 +0530

    cleaned up and starting from scratch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  teamMembers: string[] = [];
-
-  addTeamMember(memberInfo: any) {
-    const memberInfoString = `${memberInfo.name} - Skills: ${memberInfo.skills.join(", ")} - Vacation: ${memberInfo.vacation.start.toLocaleDateString()} to ${memberInfo.vacation.end.toLocaleDateString()}`;
-    this.teamMembers.push(memberInfoString);
-  }
+  title = 'ProjectManning';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TeamInputComponent } from './team-input/team-input.component';
-import { TeamInfoComponent } from './team-info/team-info.component';
-import { CalendarComponent } from './calendar/calendar.component';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TeamInputComponent,
-    TeamInfoComponent,
-    CalendarComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
